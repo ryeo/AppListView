65ce3c32276fd48f078502eff958093aa45764f9
ryeo <robertyeo@hotmail.com>
2013-04-11 15:40:48 -0700

    added customlistview

---
+++ src/com/example/applistview/CustomizedListView.java
@@ -0,0 +1,77 @@
+package com.example.applistview;
+
+
+import com.fedorvlasov.lazylist;
+import java.util.ArrayList;
+import java.util.HashMap;
+ 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+ 
+public class CustomizedListView extends Activity {
+    // All static variables
+    static final String URL = "http://api.androidhive.info/music/music.xml";
+    // XML node keys
+    static final String KEY_SONG = "song"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_TITLE = "title";
+    static final String KEY_ARTIST = "artist";
+    static final String KEY_DURATION = "duration";
+    static final String KEY_THUMB_URL = "thumb_url";
+ 
+    ListView list;
+    LazyAdapter adapter;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+ 
+        ArrayList&lt;HashMap&lt;String, String&gt;&gt; songsList = new ArrayList&lt;HashMap&lt;String, String&gt;&gt;();
+ 
+        XMLParser parser = new XMLParser();
+        String xml = parser.getXmlFromUrl(URL); // getting XML from URL
+        Document doc = parser.getDomElement(xml); // getting DOM element
+ 
+        NodeList nl = doc.getElementsByTagName(KEY_SONG);
+        // looping through all song nodes &lt;song&gt;
+        for (int i = 0; i &lt; nl.getLength(); i++) {
+            // creating new HashMap
+            HashMap&lt;String, String&gt; map = new HashMap&lt;String, String&gt;();
+            Element e = (Element) nl.item(i);
+            // adding each child node to HashMap key =&gt; value
+            map.put(KEY_ID, parser.getValue(e, KEY_ID));
+            map.put(KEY_TITLE, parser.getValue(e, KEY_TITLE));
+            map.put(KEY_ARTIST, parser.getValue(e, KEY_ARTIST));
+            map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
+            map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+ 
+            // adding HashList to ArrayList
+            songsList.add(map);
+        }
+ 
+        list=(ListView)findViewById(R.id.list);
+ 
+        // Getting adapter by passing xml data ArrayList
+        adapter=new LazyAdapter(this, songsList);
+        list.setAdapter(adapter);
+ 
+        // Click event for single list row
+        list.setOnItemClickListener(new OnItemClickListener() {
+ 
+            @Override
+            public void onItemClick(AdapterView&lt;?&gt; parent, View view,
+                    int position, long id) {
+ 
+            }
+        });
+    }
+}
